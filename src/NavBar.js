e6c356a3e5a8199a5a2319ed79db3b62ff413b62
christine <bumpouce@gmail.com>
2020-08-19 13:34:35 -0700

    logo!

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+
+class Navigation extends Component {
+    render (){
+        return (
+            <div>
+                <h1></h1>
+            </div>
+        )
+    }
+}
+
+export default Navigation
