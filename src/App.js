3b909015618b50288b37267c34affa76a20ab26b
christine <bumpouce@gmail.com>
2020-08-19 17:10:38 -0700

    Logo and nav bar layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect} from "react-router-dom"
-import logo from './bookdiary.png';
 import './App.css';
 import Body from './Body.js'
+import Navigation from './NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* className="App-logo"  */}
-        <img src={logo} alt="logo" /> 
-        <Body />
-      </header>
+      <Navigation />
+      <Body />
     </div>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
+import logo from './bookdiary.png';
 
 class Navigation extends Component {
     render (){
         return (
-            <div>
-                <h1></h1>
-            </div>
+            <span class="App-header">
+                <img src={logo} alt="logo" class="logo"/> <br></br>
+                <ul class="nav">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">History</a></li>
+                    <li><a href="">Update</a></li>
+                    <li><a href="">Quotes</a></li>
+                    <li><a href="">Favorites</a></li>
+                    <li><a href="">Suggestions</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </span>
         )
     }
 }
 
 export default Navigation
+
+// Image by <a href="https://pixabay.com/users/kropekk_pl-114936/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=287140">kropekk_pl</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=287140">Pixabay</a>
